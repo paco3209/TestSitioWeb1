aa4178d1641171bed1c185df941aef5bf0f62054
fran <paco3209@hotmail.com>
2012-09-15 18:47:54 -0300

    prueba1509

--- inicio.php
+++ inicio.php
@@ -10,7 +10,7 @@ do_html_header('Prueba');
 
 presentacion_navegador();
 link_nuevo_registro();
-presentacion_noticias("SELECT * FROM Noticias order by Votos desc");
+presentacion_noticias("SELECT id_noticia, id_usuario, Noticias.descripcion, Fecha, votos, titulo, link_noticia, categoria, imagen_categoria FROM Noticias, categoria where Noticias.Categoria = categoria.descripcion order by Votos desc");
 presentacion_publicidad();
 ir_al_cielo();
 
